35d794cfe6022da028e4e7fd90bedde66dd26fe0
burungberenang <burungberenang@gmail.com>
2014-07-30 15:37:07 +0700

    A+ Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new APositive\TestBundle\APositiveTestBundle(),
+            new APositive\Bundle\APositiveBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
